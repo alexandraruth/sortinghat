f01fcb84edae7edf4ed5f348f22d541d26596d47
alexandraruth <66081217+alexandraruth@users.noreply.github.com>
2021-03-14 14:11:20 -0400

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,30 +1,34 @@
 #
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+# This is the user-interface code for sortinghat
 
 library(shiny)
 
-# Define UI for application that draws a histogram
+# Define UI 
 shinyUI(fluidPage(
 
     # Application title
     titlePanel("Hogwarts Sorting Hat"),
 
-    # Sidebar with a slider input for number of bins
+    # Sidebar 
     sidebarLayout(
         sidebarPanel(
             p("There\'s nothing hidden in your head the sorting hat can\'t see,"),
-            p('So try me on and I will tell you where you ought to be.')
+            p('So try me on and I will tell you where you ought to be.'),
+            
+            radioButtons("sorting", label = h3("Which of these best describes your heart?"),
+                         choices = list("Brave" = "GRYFFINDOR!", "Loyal" = "HUFFLEPUFF!", "Wise" = "RAVENCLAW!", "Malevolent"= "SLYTHERIN!"), 
+                        selected = 0
+                         ),
+            
+            hr()
+            
         ),
 
-        # Show a plot of the generated distribution
+        # Show a sorting hat and sorting output
         mainPanel(
-            img(src = "hat.png", height = 200, width = 200)
+            img(src = "hat.png", height = 200, width = 200),
+            
+            fluidRow(column(5, verbatimTextOutput("value")))
         )
     )
 ))
